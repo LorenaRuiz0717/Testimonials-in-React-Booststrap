03bbf725a75741bd329b834de3737cfcb1b52bd6
LorenaRuiz <lorena0717@hotmail.com>
2022-05-27 08:12:35 -0500

    add data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Testimonials from './components/Testimonials'
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 
+
+
 function App() {
   return (
     <div className='page'>
@@ -16,21 +18,17 @@ function App() {
         <h1>Labor Social</h1>
         <h5>Hola bienvenid@, aqui encontraras historias con finales felices, gaticas embarazadas en condición de calle rescatadas para darle a ella y a sus bebés una excelente calidad de vida al igual que gatitos bebes recuperados.
           Hoy en día entregados a lindos hogares donde disfrutan de amor, cuidado y compañia siendo los más consentidos del hogar.</h5>
+  
+    
         <div className='row'>
           <div className="col-12 col-lg-9">
             <div className='row'>
               <div className="col-12 col-lg-7 p-3">
                 <article className="card h-100 cardColor1">
-                  <Testimonials
-                    name='Emma'
-                    image='imgTestimonial4'
-                    city='Bogotá'
-                    testimonials='Lindos productos happy pets'
-                    description='Lorem ipsum dolor sit amet, consectetur adipiscing.'
-                  />
+                  <Testimonials/>
                 </article>
               </div>
-              <div className="col-12 col-lg-5 p-3">
+              {/* <div className="col-12 col-lg-5 p-3">
                 <article className="card h-100 cardColor2">
                   <Testimonials
                     name='Emma'
@@ -40,8 +38,8 @@ function App() {
                     description='Lorem ipsum dolor sit amet, consectetur adipiscing.'
                   />
                 </article>
-              </div>
-              <div className="col-12 col-lg-5 p-3">
+              </div> */}
+              {/* <div className="col-12 col-lg-5 p-3">
                 <article className="card h-100 cardColor3">
                   <Testimonials
                     name='Emma'
@@ -51,8 +49,8 @@ function App() {
                     description='Lorem ipsum dolor sit amet, consectetur adipiscing.'
                   />
                 </article>
-              </div>
-              <div className='col-12 col-lg-7 p-3'>
+              </div> */}
+              {/* <div className='col-12 col-lg-7 p-3'>
                 <article className="card h-100 cardColor1">
                   <Testimonials
                     name='Emma'
@@ -62,11 +60,11 @@ function App() {
                     description='Lorem ipsum dolor sit amet, consectetur adipiscing.'
                   />
                 </article>
-              </div>
+              </div> */}
             </div>
 
           </div>
-          <div className='col-12 col-lg-3 p-3'>
+          {/* <div className='col-12 col-lg-3 p-3'>
             <article className="card h-100 cardColor2">
               <Testimonials
                 name='Emma'
@@ -76,8 +74,8 @@ function App() {
                 description='Lorem ipsum dolor sit amet, consectetur adipiscing.'
               />
             </article>
-          </div>
-        </div>
+          </div> */}
+        </div> 
       </div>
     </div>
     <Footer></Footer>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 // import imgTestimonial from '../images/imgTestimonial.jpg'
 import "../styles/Testimonials.css";
+import data from "../data.json";
 
 function Testimonials(props) {
   return (
     <div className="card-body">
-      <div className="d-flex mb-3 containerImg">
-        <figure> 
-        
-          <img
-            className="border-primary border-4 imgTestimonials"
-            src={require(`../images/${props.image}.jpg`)}
-            alt="imgProduct"
-          />
-        </figure>
-      </div>
+      {data.map((dat) => {
+        return (
+          <div key={dat.id}>
+            {dat.Name}
+            <div className="d-flex mb-3 containerImg">
+              <figure>
+                <img
+                  className="border-primary border-4 imgTestimonials"
+                  src={dat.Img}
+                  alt="imgProduct"
+                />
+              </figure>
+            </div>
+          </div>
+        );
+      })}
+
       <div>
         <div className="ps-3 containerInfo">
           <h6 className="m-0 h6">
@@ -24,7 +32,6 @@ function Testimonials(props) {
           <p className="m-0 h6">{props.testimonials}</p>
           <p className="fs-6 lh-1">{props.description}</p>
         </div>
-       
       </div>
     </div>
   );
